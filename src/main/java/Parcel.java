6f7fb15cf13b63578c23c767b182a5442db51761
Kyle Derrick and trevor elvey <trevor.elvey@gmail.com>
2016-02-15 14:18:45 -0800

    add spec/code for getVolume and costToShip method

--- src/main/java/Parcel.java
+++ src/main/java/Parcel.java
@@ -1,3 +1,26 @@
 public class Parcel {
+  private int mLength;
+  private int mWidth;
+  private int mHeight;
+  private int mWeight;
+  private int mVolume;
 
+  public Parcel(int length, int width, int height, int weight) {
+    mLength = length;
+    mWidth = width;
+    mHeight = height;
+    mWeight = weight;
+  }
+
+
+  public Integer getVolume() {
+    return mVolume = mLength * mWidth * mHeight;
+  }
+
+
+
+  public Integer costToShip() {
+    Integer shippingCost = getVolume() * mWeight;
+    return shippingCost;
+  }
 }
--- src/test/java/ParcelTest.java
+++ src/test/java/ParcelTest.java
@@ -5,9 +5,20 @@ public class ParcelTest {
 
   @Test
   public void testParcel_instantiatesCorrectly() {
-    Parcel testParcel = new Parcel(3,4);
+    Parcel testParcel = new Parcel(3,4,1,10);
     assertEquals(true, testParcel instanceof Parcel);
+  }
 
+  @Test
+  public void testParcel_getVolumeOfParcel_12() {
+    Parcel testParcel = new Parcel(3,4,1,10);
+    assertEquals(12,(int) testParcel.getVolume());
+  }
+
+  @Test
+  public void testParcel_costToShip_() {
+    Parcel testParcel = new Parcel(3,4,1,10);
+    assertEquals(120,(int) testParcel.costToShip());
   }
 
 }
