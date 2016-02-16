f20092cd04f8a298e26a360c8157d7888741ded9
Kyle Derrick and trevor elvey <kderrick1988@gmail.com>
2016-02-15 16:18:36 -0800

    WIP: add front end functionality without username carrying over

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,11 +6,48 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-
-
-
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
+
+
+    get("/" , (request, response) -> {
+      HashMap<String,Object> model = new HashMap<String,Object>();
+      model.put("username", request.session().attribute("username"));
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/welcome", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      String inputtedUserName = request.queryParams("username");
+      request.session().attribute("username", inputtedUserName);
+      model.put("username", inputtedUserName);
+
+      model.put("template", "templates/welcome.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/results", (request, response) -> {
+      HashMap<String,Object> model = new HashMap<String,Object>();
+      request.session().attribute("username", username);
+      model.put("username", username);
+      int height = Integer.parseInt(request.queryParams("height"));
+      int width = Integer.parseInt(request.queryParams("width"));
+      int length = Integer.parseInt(request.queryParams("length"));
+      int weight = Integer.parseInt(request.queryParams("weight"));
+
+      Parcel myParcel = new Parcel (length, width, height, weight);
+      Integer result = myParcel.costToShip();
+
+      model.put("result", result);
+      model.put("template", "templates/results.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    }
+
+
 
         //RESTful ARCHITECTURE
         //Use POST to create something on the server
@@ -34,4 +71,3 @@ public class App {
         //ROUTES: Changing Resources
 
     }
-}
